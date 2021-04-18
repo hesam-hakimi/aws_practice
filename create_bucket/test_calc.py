5b16a4ad441b2b24a451b9ce357f4adec1579d9f
hesam hakimi <hessam.hakimi@gmail.com>
2021-04-18 15:33:58 -0400

    test

--- create_bucket/test_calc.py
+++ create_bucket/test_calc.py
@@ -7,3 +7,11 @@ class TestSample(unittest.TestCase):
     def test_add(self):
         self.assertEqual(2+2, 4)
         self.assertEqual(5, 5)
+
+    def test_add2(self):
+        self.assertEqual(2+2, 4)
+        self.assertEqual(5, 5)
+
+    def test_add3(self):
+        self.assertEqual(2+2, 4)
+        self.assertEqual(6, 5)
